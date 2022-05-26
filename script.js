e332095b775c20189e79308b03c84ad27ddb734d
Vietsalt <vietprock@gmail.com>
2022-05-26 18:21:03 +0700

    responsive xl ok

---
+++ script.js
@@ -0,0 +1,8 @@
+let loginScreenElement=document.getElementsByClassName("login_screen")
+let loginIconElement=document.getElementsByClassName("login_icon")
+
+loginIconElement.onclick=function(){
+  
+  console.log("hihi")
+}
+
